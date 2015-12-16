eb901e1c4bd8c0204d820283f2684afdc63253a3
enochcheung <toxicf@gmail.com>
2015-12-16 17:48:52 -0500

    Cleanup day16

--- day16/day16_2.hs
+++ day16/day16_2.hs
@@ -1,7 +1,6 @@
 import Data.Map.Lazy (Map)
 import qualified Data.Map.Lazy as Map
 import Text.Regex.PCRE
-import Data.Maybe (fromMaybe)
 
 
 type Sue = Map String Int
@@ -20,15 +19,15 @@ reference :: Sue
 reference = Map.fromList [("children", 3), ("cats", 7), ("samoyeds", 2), ("pomeranians", 3), ("akitas", 0), ("vizslas", 0), ("goldfish", 5), ("trees", 3), ("cars", 2), ("perfumes", 1)]
 
 parseLine :: String -> Sue
-parseLine l = Map.fromList $ map (\ [_,k,v] -> (k, read v)) match
+parseLine l = Map.fromList $ map (\ [_,k,v] -> (k, read v)) m
                where
                  pattern = "(\\w+): (\\d+)"
-                 match = l =~ pattern :: [[String]]
+                 m = l =~ pattern :: [[String]]
 
 matchSue :: Sue -> Sue -> Bool
 matchSue pattern sue = all (==True) $ Map.mapWithKey match' pattern
                      where
-                       match' k v = fromMaybe True $ fmap (match'' k v) v'
+                       match' k v = maybe True (match'' k v) v'
                           where v' = Map.lookup k sue
                        match'' "cats" = (<)
                        match'' "trees" = (<)
